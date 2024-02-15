45727ec967e834dfefcda91f1875c1ff1c95b5b1
EnzoZirotti <zirotti32@gmail.com>
2024-02-15 16:31:45 -0700

    update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
 
-var speed: float = 0.0
+@export var speed: float = 0.0
 
 const WALK_SPEED: float = 10.0
 const SPRINT_SPEED: float = 10.0
 const JUMP_VELOCITY: float = 10
-const SENSITIVITY: float = 0.001
+@export var SENSITIVITY: float = 0.001
 # Bob frequency
 const BOB_FREQ: float = 2.0
 const BOB_AMP: float = 0.08
@@ -24,17 +24,37 @@ var pull_power = 4
 # Health
 var health: int = 3
 
+var _is_crouching : bool = false
+var Couch_speed : float = 7.0
+var _is_sliding : bool = false
+var Slide_distance : float = 7.0
+
+
+const HIT_STAGGER = 5.2
 
 signal health_changed(health_value)
 
+
+@export var TOGGLE_CROUCH : bool = true
+
 @onready var head = $Head
-@onready var camera = $Head/Camera3D
 @onready var sight = $Head/Camera3D/sight
 @onready var hand = $Head/Hand
 @onready var touch = $Head/Hand/touch
-@onready var anim_player = $AnimationPlayer
 @onready var muzzle_flash = $Head/Camera3D/Pistol/MuzzleFlash
+@onready var walkingsound = $Walking
+@onready var jumpingsound = $Jumping
+@onready var runningsound = $Running
+@onready var shootingsount = $Shooting
 
+
+
+@export var crouch_shapecast : ShapeCast3D
+@export var anim_player : AnimationPlayer
+@export var camera : Camera3D
+
+
+signal player_hit
 
 
 func pick_object():
@@ -57,6 +77,9 @@ func _ready():
 	if not is_multiplayer_authority(): return
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	crouch_shapecast.add_exception($".")
+
 
 	camera.current = true
 
@@ -75,11 +98,38 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("shoot") \
 			and anim_player.current_animation != "shoot":
 		play_shoot_effects.rpc()
+		
 		if sight.is_colliding():
 			var hit_player = sight.get_collider()
 			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 
+func _input(event):
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		if event is InputEventMouseMotion:
+			head.rotate_y(-event.relative.x * SENSITIVITY)
+			camera.rotate_x(-event.relative.y * SENSITIVITY)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+			
+	if event.is_action_pressed("crouch") and is_on_floor():
+		toggle_crouch()
+			
+	
+	if direction.length() > 0.0 and is_on_floor():
+		if not walkingsound.is_playing():
+			walkingsound.play()
+	elif direction.length() == 0 or jumpingsound.is_playing():
+		walkingsound.stop()
+		runningsound.stop()
+		
 func _physics_process(_delta):
 	if not is_multiplayer_authority(): return
 	
@@ -91,10 +141,17 @@ func _physics_process(_delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		if runningsound.is_playing():
+			runningsound.stop()
+		jumpingsound.play()
 
 	# Handle Sprint
-	if Input.is_action_pressed("sprint"):
+	if Input.is_action_pressed("sprint") and is_on_floor():
+		if not runningsound.is_playing():
+			runningsound.play()
 		speed = SPRINT_SPEED
+		
+		walkingsound.stop()
 	else:
 		speed = WALK_SPEED
 
@@ -112,8 +169,6 @@ func _physics_process(_delta):
 		velocity.x = lerp(velocity.x, direction.x * speed, _delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, _delta * 3.0)
 
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 #Part of picking up
 	if Input.is_action_just_pressed("pickup"):
 		if picked_object == null:
@@ -143,12 +198,26 @@ func _physics_process(_delta):
 		anim_player.play("idle")
 		
 	move_and_slide()
+	
+func toggle_crouch():
+	if _is_crouching == true and crouch_shapecast.is_colliding() == false:
+		anim_player.play("Crouch", -1 , -Couch_speed, true)
+	elif _is_crouching == false:
+		anim_player.play("Crouch", -1 , Couch_speed)
+		
+func _on_animation_player_animation_started(anim_name):
+	if anim_name == "Crouch":
+		_is_crouching = !_is_crouching
 
 func _headbob(time: float) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
 	return pos
+	
+func hit(dir):
+	emit_signal("player_hit")
+	velocity += dir * HIT_STAGGER
 
 @rpc("call_local")
 func play_shoot_effects():
